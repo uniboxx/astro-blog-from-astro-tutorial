772a8dd407bc6151751a6f15e9ace46f86fd6519
unibox <unibox@duck.com>
2025-01-28 12:55:40 +0100

    first commit with bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://uniboxx.github.io',
+  base: '/astro_01',
+});
