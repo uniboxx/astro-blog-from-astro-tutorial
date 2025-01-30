f889391dba51167d55ccfb760953749afb5d2baf
unibox <unibox@duck.com>
2025-01-30 11:50:06 +0100

    change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://uniboxx.github.io',
-  base: '/astro_01',
+  base: '/astro-blog-from-astro-tutorial',
 
   integrations: [preact()],
 });
